0adbfcb6a0f445dd3f79a0e880698d9e65bae7b2
oonamo <onam75hernandez@gmail.com>
2024-06-18 06:04:47 -0700

    ci: testing windows install

--- install.bat
+++ install.bat
@@ -11,6 +11,7 @@ echo Installing at "%INSTALL_DIR%"
 move /Y "%~dp0\creal.exe" "%INSTALL_DIR%"
 
 rem Add to path
-REM setx PATH "%PATH%;%INSTALL_DIR%"
+setx PATH "%PATH%;%INSTALL_DIR%"
+echo %PATH%
 
 echo Installed creal.
