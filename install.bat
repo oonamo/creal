e4baf4613f843faca38e9aa2473104cfaec67d9e
oonamo <onam75hernandez@gmail.com>
2024-06-18 06:42:52 -0700

    ci: check win install path

--- install.bat
+++ install.bat
@@ -9,9 +9,11 @@ if not exist "%INSTALL_DIR%\" (
     mkdir "%INSTALL_DIR%"
 )
 
+if not exists "%~dp0creal.exe" make build
+
 rem Copy executable
 echo Installing at "%INSTALL_DIR%\"
-move /Y "%~dp0\creal.exe" "%INSTALL_DIR%"
+copy /Y "%~dp0creal.exe" "%INSTALL_DIR%\"
 
 rem Add to path
 setx PATH "%PATH%;%INSTALL_DIR%"
