89bc92a74523b1592bad95bbfd6ea17c45a2ea0a
oonamo <onam75hernandez@gmail.com>
2024-06-21 13:41:04 -0700

    install: user %APPDATA% instead of %USERPROFILE% and remove temp file

--- install.bat
+++ install.bat
@@ -1,20 +1,23 @@
 @echo off
 
 rem define installation directory
-set INSTALL_DIR=%USERPROFILE%\creal
+set INSTALL_DIR=%APPDATA%\creal
 
 if not exist "%INSTALL_DIR%\" (
     echo making folder at "%INSTALL_DIR%"
     mkdir "%INSTALL_DIR%"
 )
 
+rem TODO: Package executable in release
 make build
 
 rem Copy executable
 echo Installing at "%INSTALL_DIR%\"
 copy /Y "%~dp0creal.exe" "%INSTALL_DIR%\"
 
-rem TODO: Fix this not adding to path.
+rem Copy path to a file
+echo %PATH% > "%INSTALL_DIR%\path.bak"
+
 set "INSTALL_DIR=%PATH%;%INSTALL_DIR%"
 reg add HKEY_CURRENT_USER\Environment /v Path /t REG_EXPAND_SZ /d "%INSTALL_DIR%" /f
 
